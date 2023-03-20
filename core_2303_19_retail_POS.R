ab284629890aa8370c96d2f880de16cbf19f8486
Henry Tu <tutrunghieu@gmail.com>
2023-03-20 09:08:48 +0800

    Update core_2303_19_retail_POS.R
    Fixing bridge colors

--- core_2303_19_retail_POS.R
+++ core_2303_19_retail_POS.R
@@ -23,7 +23,7 @@ fmt_c1_e9 <- function(x, div=1e9) { format(round(x/div, 1), nsmall=1, big.mark="
 
 #-------------------------------------------
 draw_bridge_YTD <- function(df=rename(dataset, "xx", "yy", "fill", "tag"), more=NULL, 
-color_mapping=list(major='yellow', hypermarkets='red', supermarkets='green', others='cyan'), 
+color_mapping=list(major='yellow', hypermarkets='pink', supermarkets='magenta', others='cyan'), 
 box_width=0.47, yy_fmt=fmt_c1_e3, fmt=fmt_c1_e3, xx_angle=25, major_mult=0.5, major_angle=0, minor_angle=25, legend=TRUE) {
     g <- ggplot(df);
     g <- g + geom_text(aes(x=xx, y=0, label='', group=fill), stat="identity", show.legend=FALSE);
@@ -98,3 +98,33 @@ add_lines_YTD <- function(g, df, more=NULL) {
     g <- g + scale_fill_manual(values=map);
     return(g);
 }
+
+#-------------------------------------------
+draw_carousel_chart1 <- function(ddd, more) { 
+    g <- ggplot(ddd) + geom_bar(aes(x=xx, y=yy, fill=fill), stat="identity", show.legend=more$legend); 
+    g <- g + theme(axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.x = element_text(angle=more$xx_angle) );
+    g <- g + scale_y_continuous(labels=more$yy_fmt);
+    if( !is.null(more$color_mapping) ) g <- g + scale_fill_manual(values=more$color_mapping);
+    return(g);
+}
+
+#-------------------------------------------
+draw_carousel_chart2 <- function(ddd, more) {  
+    g <- ggplot(ddd) + geom_bar(aes(x=xx, y=yy, fill=fill), stat="identity", position="fill", show.legend=more$legend); 
+    g <- g + theme(axis.title.x = element_blank(), axis.title.y = element_blank(), axis.text.x = element_text(angle=more$xx_angle) );
+#    g <- g + scale_y_continuous(labels=more$yy_fmt);
+    if( !is.null(more$color_mapping) ) g <- g + scale_fill_manual(values=more$color_mapping);
+    return(g);
+}
+
+#-------------------------------------------
+draw_carousel_SF <- function(df=rename(dataset, "xx", "yy", "fill"), more=NULL) {
+    if( is.null(more) ) {
+        more <- list(chart1=draw_carousel_chart1, chart2=draw_carousel_chart2, array_ncol=2, xx_angle=15, yy_fmt=fmt_c1_e3, legend=TRUE);
+	more$color_mapping <- list('A, top 10 customers'='yellow', 'B, other customers'='grey', 'A, top 10 stores'='yellow', 'B, other stores'='grey')
+    }
+    
+    ldf <- list(stack=more$chart1(df, more), full=more$chart2(df, more));
+    grid.arrange(grobs=ldf, ncol=more$array_ncol);
+}
+
