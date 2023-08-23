c7cf6b58ef8c28bc1ae3b719e72f7349c2686ae5
CT952KF <CT952KF@SG2236511W2.ey.net>
2023-08-23 13:41:58 +0800

    Adding legend

--- 2308/waterfall-enum.R
+++ 2308/waterfall-enum.R
@@ -1,11 +1,11 @@
 
-waterfall_enum <- function(wdf, pos='bottom', lab='major', size=0.45, ncol=2) { 
+waterfall_enum <- function(wdf, pos='bottom', lab='major', size=0.45, legend=FALSE, ncol=2) { 
     ldf <- split(wdf, wdf$tile);
     ldx <- list();
 
     for(nk in names(ldf)) {
         df <- waterfall_data(ldf[[nk]], lab=lab, size=size);
-        ldx[[nk]] <- waterfall_chart(df, pos=pos) + facet_wrap(ncol=1, tile ~ .);
+        ldx[[nk]] <- waterfall_chart(df, pos=pos, legend=legend) + facet_wrap(ncol=1, tile ~ .);
     }
 
     grid.arrange(grobs=ldx, ncol=ncol);
