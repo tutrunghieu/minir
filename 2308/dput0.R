e5ed7d8a7fca66a2759e8fd033a56e39576fde07
CT952KF <CT952KF@SG2236511W2.ey.net>
2023-08-25 20:31:33 +0800

    adding dput0

---
+++ 2308/dput0.R
@@ -0,0 +1,30 @@
+
+
+#-------------------------------------
+dput0 <- function() {
+
+    vars <- list();
+    
+    vars$include <- function() {
+        library(openssl);
+        library(ggplot2);
+        library(openssl);
+
+        source_md5 <- function(uk, tar=NULL) {
+            if( is.null(tar) ) tar <- file.path(Sys.getenv("USERPROFILE"), '.out', paste0(md5(uk), ".R"));
+            dir.create(dirname(tar), recursive=TRUE, showWarnings = FALSE); 
+            if( !file.exists(tar) ) download.file(uk, tar);
+    		source(tar);
+        }
+
+        source_md5('https://raw.githubusercontent.com/tutrunghieu/minir/main/2308/sorted-waterfall.R');
+        source_md5('https://raw.githubusercontent.com/tutrunghieu/minir/main/2308/split-gantt.R');
+        source_md5('https://raw.githubusercontent.com/tutrunghieu/minir/main/2308/waterfall-enum.R');
+    }
+
+    dput(vars, file=file.path(Sys.getenv("USERPROFILE"), '.out', "project-settings.R"));
+}
+
+#-------------------------------------
+dput0();
+library(ggplot2); print(ggplot() + ggtitle("Saved!!!") );
