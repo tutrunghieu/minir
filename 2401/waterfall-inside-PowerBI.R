1f4d3798e34080737caebd20e5e4e3abc9258baf
Henry Tu <tutrunghieu@gmail.com>
2024-01-12 13:58:09 +0800

    Update waterfall-inside-PowerBI.R
    Updating text angle

--- 2401/waterfall-inside-PowerBI.R
+++ 2401/waterfall-inside-PowerBI.R
@@ -18,7 +18,7 @@ waterfall_array <- function(gdf, ncol=2, tile_vals=scale_factor_USUK) {
     grid.arrange(grobs=ldf, ncol=ncol);
 }
 
-waterfall_chart <- function(gdf, width=0.45, text_fmt=fmt_c1_e3, text_vert=+1, left_fmt=scale_cy_c1_e3) {
+waterfall_chart <- function(gdf, width=0.45, text_fmt=fmt_c1_e3, text_vert=+1, text_ang=25, left_fmt=scale_cy_c1_e3) {
     s <- 0;
     for(k in 1:nrow(gdf)) {
         if( gdf$fill[k] == "major") { gdf[k, "y1"] <- 0; gdf[k, "y2"] <- s <- gdf$amt[k]; gdf[k, "ym"] <- gdf[k, "y2"]/2; }
@@ -32,7 +32,7 @@ waterfall_chart <- function(gdf, width=0.45, text_fmt=fmt_c1_e3, text_vert=+1, l
 
 #    g <- g + geom_bar(aes(x=FY_long, y=amt, fill=fill), stat="identity", show.legend=FALSE);
    g <- g + geom_rect(aes(xmin=x1, xmax=x2, ymin=y1, ymax=y2, fill=fill), show.legend=FALSE);
-   g <- g + geom_text(aes(x=(x1+x2)/2, y=ym, label=text_fmt(amt)), vjust=text_vert, show.legend=FALSE);
+   g <- g + geom_text(aes(x=(x1+x2)/2, y=ym, label=text_fmt(amt)), angle=text_ang, vjust=text_vert, show.legend=FALSE);
 
     g <- g + theme(axis.text.x = element_text(angle=25), axis.title.x = element_blank(), axis.title.y = element_blank() );
     g <- g + scale_x_discrete(labels=gdf$FY_short) + scale_fill_PVM() + left_fmt();
